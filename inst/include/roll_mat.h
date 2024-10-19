6d414306e5ba1577bc19a8f09e300d3152d849bc
Jason Foster <jason.j.foster@gmail.com>
2024-10-19 14:13:35 -0400

    Version: 1.1.8

--- inst/include/roll_mat.h
+++ inst/include/roll_mat.h
@@ -859,6 +859,8 @@ struct RollMeanOnlineMat : public Worker {
     for (std::size_t j = begin_col; j < end_col; j++) {
       
       int n_obs = 0;
+      bool is_na = false;
+      bool is_na_old = false;
       long double lambda = 0;
       long double w_new = 0;
       long double w_old = 0;
@@ -867,98 +869,86 @@ struct RollMeanOnlineMat : public Worker {
       long double sum_w = 0;
       long double sum_x = 0;
       
-      if (width > 1) {
-        if (n > 1) {
-          lambda = arma_weights[n - 2] / arma_weights[n - 1]; // check already passed
-        } else {
-          lambda = arma_weights[n - 1];
-        }
-      } else {
-        lambda = arma_weights[n - 1];
-      }
-      
       for (int i = 0; i < n_rows_x; i++) {
         
-        if ((arma_any_na[i] != 0) || std::isnan(x(i, j))) {
-          
-          w_new = 0;
-          x_new = 0;
-          
-        } else {
-          
-          w_new = arma_weights[n - 1];
-          x_new = x(i, j);
-          
+        is_na = (arma_any_na[i] != 0) || std::isnan(x(i, j));
+        
+        if (i >= width) {
+          is_na_old = (arma_any_na[i - width] != 0) || (std::isnan(x(i - width, j)));
         }
         
-        // expanding window
-        if (i < width) {
+        update_n_obs(n_obs, is_na, is_na_old, i, width);
+        
+        if (width > 1) {
           
-          // don't include if missing value and 'any_na' argument is 1
-          // note: 'any_na' is set to 0 if 'complete_obs' argument is FALSE
-          if ((arma_any_na[i] == 0) && !std::isnan(x(i, j))) {
-            n_obs += 1;
+          if (n > 1) {
+            lambda = arma_weights[n - 2] / arma_weights[n - 1]; // check already passed
+          } else {
+            lambda = arma_weights[n - 1];
           }
           
-          if (width > 1) {
+          if (!is_na) {
+            
+            w_new = arma_weights[n - 1];
+            x_new = x(i, j);
+            
+          } else {
+            
+            w_new = 0;
+            x_new = 0;
+            
+          }
+          
+          // expanding window
+          if (i < width) {
             
             sum_w = lambda * sum_w + w_new;
             sum_x = lambda * sum_x + w_new * x_new;
             
+            // rolling window
           } else {
             
-            sum_w = w_new;
-            sum_x = w_new * x_new;
-            
-          }
-          
-        }
-        
-        // rolling window
-        if (i >= width) {
-          
-          // don't include if missing value and 'any_na' argument is 1
-          // note: 'any_na' is set to 0 if 'complete_obs' argument is FALSE
-          if ((arma_any_na[i] == 0) && !std::isnan(x(i, j)) &&
-              ((arma_any_na[i - width] != 0) || std::isnan(x(i - width, j)))) {
-            
-            n_obs += 1;
-            
-          } else if (((arma_any_na[i] != 0) || std::isnan(x(i, j))) &&
-            (arma_any_na[i - width] == 0) && !std::isnan(x(i - width, j))) {
-            
-            n_obs -= 1;
-            
-          }
-          
-          if ((arma_any_na[i - width] != 0) || std::isnan(x(i - width, j))) {
-            
-            w_old = 0;
-            x_old = 0;
-            
-          } else {
-            
-            w_old = arma_weights[n - width];
-            x_old = x(i - width, j);
-            
-          }
-          
-          if (width > 1) {
+            if (!is_na_old) {
+              
+              w_old = arma_weights[n - width];
+              x_old = x(i - width, j);
+              
+              
+            } else {
+              
+              w_old = 0;
+              x_old = 0;
+              
+            }
             
             sum_w = lambda * sum_w + w_new - lambda * w_old;
             sum_x = lambda * sum_x + w_new * x_new - lambda * w_old * x_old;
             
+          }
+          
+        } else {
+          
+          lambda = arma_weights[n - 1];
+          
+          if (!is_na) {
+            
+            w_new = arma_weights[n - 1];
+            x_new = x(i, j);
+            
           } else {
             
-            sum_w = w_new;
-            sum_x = w_new * x_new;
+            w_new = 0;
+            x_new = 0;
             
           }
+          
+          sum_w = w_new;
+          sum_x = w_new * x_new;
           
         }
         
         // don't compute if missing value and 'na_restore' argument is TRUE
-        if ((!na_restore) || (na_restore && !std::isnan(x(i, j)))) {
+        if (!na_restore || !std::isnan(x(i, j))) {
           
           if (n_obs >= min_obs) {
             arma_mean(i, j) = sum_x / sum_w;
@@ -1014,21 +1004,23 @@ struct RollMeanOfflineMat : public Worker {
       int i = z / n_cols_x;
       int j = z % n_cols_x;
       
-      int count = 0;
-      int n_obs = 0;
-      long double sum_w = 0;
-      long double sum_x = 0;
-      
       // don't compute if missing value and 'na_restore' argument is TRUE
-      if ((!na_restore) || (na_restore && !std::isnan(x(i, j)))) {
+      if (!na_restore || !std::isnan(x(i, j))) {
+        
+        int n_obs = 0;
+        bool is_na = false;
+        long double sum_w = 0;
+        long double sum_x = 0;
         
         // number of observations is either the window size or,
         // for partial results, the number of the current row
-        while ((width > count) && (i >= count)) {
+        for (int count = 0; (count < width) && (count <= i); count++) {
           
           // don't include if missing value and 'any_na' argument is 1
           // note: 'any_na' is set to 0 if 'complete_obs' argument is FALSE
-          if ((arma_any_na[i - count] == 0) && !std::isnan(x(i - count, j))) {
+          is_na = (arma_any_na[i - count] != 0) || std::isnan(x(i - count, j));
+          
+          if (!is_na) {
             
             // compute the sum
             sum_w += arma_weights[n - count - 1];
@@ -1036,8 +1028,6 @@ struct RollMeanOfflineMat : public Worker {
             n_obs += 1;
             
           }
-          
-          count += 1;
           
         }
         
